1f763621900bbfc9e89f44d783828b23463fc1b3
Paul E. Lovato <plovato@gmail.com>
2019-08-29 18:54:57 -0600

    Basic data.

---
+++ html/resources/views/bruces/home.blade.php
@@ -0,0 +1,36 @@
+@extends('bruces.layouts.app')
+
+@section('title', 'Welcome')
+
+@section('content')
+    <div class="container">
+        <section class="content-section about-inc">
+            <h3>About Bruce's Installations, Inc.</h3>
+            <p>We specialize in professional audio, video, surveillance and network solutions for both home and
+                commercial applications in the Southern and Central regions of California. Our principle values are
+                honesty, integrity, professionalism and quality of work.</p>
+        </section>
+
+        <section class="content-section about-owner">
+            <h3>About the Owner</h3>
+            <p>Bruce Montano has been in the professional installation industry for over fifteen years which includes
+                long-time residences with Adelphia, Time Warner, and Spectrum Cable. Bruce believes that healthy
+                customer relationships are best supported by operational integrity, sincerity and dependability.</p>
+        </section>
+
+        <section class="content-section types-of-installs">
+            <h3>Types Of Installations (Home & Commercial/Business)</h3>
+            <ul>
+                <li>TV Mounting</li>
+                <li>A/V Cable Installation & Concealment/Wall Fishing</li>
+                <li>Wired Network Configuration</li>
+                <li>Wireless Network Configuration</li>
+                <li>Surveillance and Remote Monitoring Systems</li>
+                <li>Alarm Systems</li>
+                <li>Whole-Home Smart Solution Installation & Configuration including lighting, thermostat, and
+                    smoke/carbon monoxide detection.
+                </li>
+            </ul>
+        </section>
+    </div>
+@endsection
---
+++ html/resources/views/bruces/layouts/app.blade.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/bruces.css">
+
+    <title>Bruce's Installations - @yield('title')</title>
+</head>
+<body>
+<div class="content-wrapper">
+    <header class="page-header">
+        <div class="container">
+            <div class="d-flex justify-content-between">
+                <h1>Bruce's Installations</h1>
+                <a href="//www.instagram.com/brucesinstallations/" target="_blank" title="Follow Us On Instagram">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram" class="svg-inline--fa fa-instagram fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg>
+                </a>
+            </div>
+        </div>
+    </header>
+
+    <main class="page-body">
+        @yield('content')
+    </main>
+
+    <footer class="page-footer">
+        <div class="container">
+            <div class="d-md-flex d-sm-block justify-content-between">
+                <a href="//www.instagram.com/brucesinstallations/" target="_blank" title="Follow Us On Instagram">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram" class="svg-inline--fa fa-instagram fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg>
+                    @brucesinstallations
+                </a>
+
+                <p class="small">&copy; <?php echo date('Y'); ?> Bruce's Installations, Inc. All Rights Reserved.</p>
+            </div>
+        </div>
+    </footer>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
