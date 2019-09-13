7c9901a364cfa59b4b3a8aaec91e9d46dccc57fc
Paul E. Lovato <plovato@gmail.com>
2019-09-13 16:52:37 -0600

    New look, almost done.

--- html/resources/views/bruces/home.blade.php
+++ html/resources/views/bruces/home.blade.php
@@ -4,33 +4,76 @@
 
 @section('content')
     <div class="container">
-        <section class="content-section about-inc">
-            <h3>About Bruce's Installations, Inc.</h3>
-            <p>We specialize in professional audio, video, surveillance and network solutions for both home and
-                commercial applications in the Southern and Central regions of California. Our principle values are
-                honesty, integrity, professionalism and quality of work.</p>
+        <section class="content-section types-of-installs">
+            <h3>Home & Business Installations</h3>
+
+            <ul class="list-unstyled">
+                <li class="media">
+                    <img src="/assets/images/tv-mounting.png" class="mr-3" alt="TV Mount icon.">
+                    <div class="media-body">
+                        <h5 class="mt-0 mb-1">TV Mounting</h5>
+                        Installations, mounts, wire hiding and more!
+                    </div>
+                </li>
+                <li class="media">
+                    <img src="/assets/images/av-cable.png" class="mr-3" alt="A/V Cable icon.">
+                    <div class="media-body">
+                        <h5 class="mt-0 mb-1">A/V Cable Installation & Concealment/Wall Fishing</h5>
+                        Your home theater system should be a source of joy — both for sound and appearance.
+                    </div>
+                </li>
+                <li class="media">
+                    <img src="/assets/images/wired-network.png" class="mr-3" alt="Wired network icon.">
+                    <div class="media-body">
+                        <h5 class="mt-0 mb-1">Wired Network Configuration</h5>
+                        A wired setup uses physical cables to transfer data between your different devices and computer systems.
+                    </div>
+                </li>
+                <li class="media">
+                    <img src="/assets/images/wireless-network.png" class="mr-3" alt="Wireless network icon.">
+                    <div class="media-body">
+                        <h5 class="mt-0 mb-1">Wireless Network Configuration</h5>
+                        Link multiple devices over a short distance through the use of wireless communication.
+                    </div>
+                </li>
+                <li class="media">
+                    <img src="/assets/images/security-camera.png" class="mr-3" alt="Security icon.">
+                    <div class="media-body">
+                        <h5 class="mt-0 mb-1">Surveillance and Remote Monitoring Systems</h5>
+                        Remote video surveillance enables you to view live video on a PC or laptop from just about anywhere in the world with internet access.
+                    </div>
+                </li>
+                <li class="media">
+                    <img src="/assets/images/alarm-icon.png" class="mr-3" alt="Alarm icon">
+                    <div class="media-body">
+                        <h5 class="mt-0 mb-1">Alarm Systems</h5>
+                        Upgrade your security and your lifestyle.
+                    </div>
+                </li>
+                <li class="media">
+                    <img src="/assets/images/smart-home.png" class="mr-3" alt="Smart home icon.">
+                    <div class="media-body">
+                        <h5 class="mt-0 mb-1">Whole-Home Smart Solution Installation & Configuration</h5>
+                        Includes lighting, thermostat, and smoke/carbon monoxide detection.
+                    </div>
+                </li>
+            </ul>
         </section>
 
         <section class="content-section about-owner">
             <h3>About the Owner</h3>
-            <p>Bruce Montano has been in the professional installation industry for over fifteen years which includes
-                long-time residences with Adelphia, Time Warner, and Spectrum Cable. Bruce believes that healthy
-                customer relationships are best supported by operational integrity, sincerity and dependability.</p>
-        </section>
 
-        <section class="content-section types-of-installs">
-            <h3>Home & Commercial/Business Installations</h3>
-            <ul>
-                <li>TV Mounting</li>
-                <li>A/V Cable Installation & Concealment/Wall Fishing</li>
-                <li>Wired Network Configuration</li>
-                <li>Wireless Network Configuration</li>
-                <li>Surveillance and Remote Monitoring Systems</li>
-                <li>Alarm Systems</li>
-                <li>Whole-Home Smart Solution Installation & Configuration including lighting, thermostat, and
-                    smoke/carbon monoxide detection.
-                </li>
-            </ul>
+            <div class="row">
+                <div class="col-md-8">
+                    <p>Bruce Montano has been in the professional installation industry for over fifteen years which includes long-time residences with Adelphia, Time Warner, and Spectrum Cable.</p>
+
+                    <p>Bruce believes that healthy customer relationships are best supported by operational integrity, sincerity and dependability.</p>
+                </div>
+
+                <div class="col-md-4">
+                    <img src="/assets/images/working-hard.jpg" class="img-fluid">
+                </div>
+            </div>
         </section>
     </div>
 @endsection
--- html/resources/views/bruces/layouts/app.blade.php
+++ html/resources/views/bruces/layouts/app.blade.php
@@ -18,12 +18,16 @@
 <body>
 <div class="content-wrapper">
     <header class="page-header">
-        <div class="container">
-            <div class="d-flex justify-content-between">
-                <h1>Bruce's Installations</h1>
-                <a href="//www.instagram.com/brucesinstallations/" target="_blank" title="Follow Us On Instagram">
-                    <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram" class="svg-inline--fa fa-instagram fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg>
-                </a>
+        <div class="bruce-hero gradient-overlay">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1 class="display-4">Bruce's Installations</h1>
+                        <p class="lead">We specialize in professional audio, video, surveillance and network solutions for both home and commercial applications in the Southern and Central regions of California. Our principle values are honesty, integrity, professionalism and quality of work.</p>
+                        <a class="btn btn-primary" href="mailto:brucescctv@gmail.com">Contact Us</a>
+                        <a class="btn btn-primary" href="tel:+18186507381">Call Bruce's Installations</a>
+                    </div>
+                </div>
             </div>
         </div>
     </header>
@@ -35,10 +39,26 @@
     <footer class="page-footer">
         <div class="container">
             <div class="d-md-flex d-sm-block justify-content-between">
-                <a href="//www.instagram.com/brucesinstallations/" target="_blank" title="Follow Us On Instagram">
-                    <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram" class="svg-inline--fa fa-instagram fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg>
-                    @brucesinstallations
-                </a>
+                <div class="footer-contact d-flex">
+                    <a class="footer-social" href="//www.instagram.com/brucesinstallations/" target="_blank"
+                       title="Follow Us On Instagram">
+                        <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram"
+                             class="svg-inline--fa fa-instagram fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg"
+                             viewBox="0 0 448 512">
+                            <path fill="currentColor"
+                                  d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path>
+                        </svg>
+                        brucesinstallations
+                    </a>
+
+                    <a class="footer-social" href="tel:+18186507381" target="_blank" title="Give us a call">
+                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone-square-alt" class="svg-inline--fa fa-phone-square-alt fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h352a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48zm-16.39 307.37l-15 65A15 15 0 0 1 354 416C194 416 64 286.29 64 126a15.7 15.7 0 0 1 11.63-14.61l65-15A18.23 18.23 0 0 1 144 96a16.27 16.27 0 0 1 13.79 9.09l30 70A17.9 17.9 0 0 1 189 181a17 17 0 0 1-5.5 11.61l-37.89 31a231.91 231.91 0 0 0 110.78 110.78l31-37.89A17 17 0 0 1 299 291a17.85 17.85 0 0 1 5.91 1.21l70 30A16.25 16.25 0 0 1 384 336a17.41 17.41 0 0 1-.39 3.37z"></path></svg>
+                        818.650.7381
+                    </a>
+                </div>
+
+
+
 
                 <p class="small">&copy; <?php echo date('Y'); ?> Bruce's Installations, Inc. All Rights Reserved.</p>
             </div>
@@ -46,8 +66,14 @@
     </footer>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </body>
 </html>
